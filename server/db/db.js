5082cb9dedfa5d2682ecd06e170a24ddfe9d3372
wyj <2879368688@qq.com>
2023-12-04 13:39:42 +0800

    Compus_Second_Hand

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://root:wyj1217qwerTYuiOP..@8.140.193.253:27017/?authSource=admin')
+mongoose.connect('mongodb://root:wyj1217qwerTYuiOP..@8.140.193.253:27017/Compus_Second_Hand?authSource=admin')
 
 const conn=mongoose.connection
 
